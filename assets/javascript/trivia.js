3732331d390dbd1c9234875947e180de1696b7b8
Jonathan Chin <jonathankchin@gmail.com>
2016-12-12 19:29:27 +0000

    working on creating object to store questions and answers || made a few changes to css and html

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,24 +1,30 @@
+/* global $ */
+// for cloud9 IDE purposes
 $(document).ready(function(){
    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
    $('#instructionModal').modal();
 
    $('.tooltipped').tooltip({delay: 50});
 
-   var questions = [
-     {
-        "question" : "How big is the su?",
-        "choices" : [ "To get to the otherside", "Get some lemonade", "Go to the store"],
-        "answer": this.choices[0]
-     },
-     {
-        "question" : "How many rings does Saturn have?",
-        "choices" : [ "To get to the otherside", "Get some lemonade", "Go to the store"],
-        "answer": this.choices[0]
-     },
-     
+//   var questions = [
+//      {
+//         "question" : "How big is the su?",
+//         "choices" : [ "To get to the otherside", "Get some lemonade", "Go to the store"],
+//         "answer": this.choices[0]
+//      },
+//      {
+//         "question" : "How many rings does Saturn have?",
+//         "choices" : [ "To get to the otherside", "Get some lemonade", "Go to the store"],
+//         "answer": this.choices[0]
+//      },
+//      {
+//         "question" : "How many rings does Saturn have?",
+//         "choices" : [ "To get to the otherside", "Get some lemonade", "Go to the store"],
+//         "answer": this.choices[0]
+//      }
+//   ]
 
-   ]
-
+$('#question').append("<div>Paragraph</div>");
 
 
 
