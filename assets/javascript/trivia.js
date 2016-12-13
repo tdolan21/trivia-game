1b5743f6ce1dcf98c8638804093a3b48fac99a97
chinjon <Jonathankchin@gmail.com>
2016-12-13 14:34:33 -0500

    working through functionality

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,5 +1,6 @@
-/* global $ */
-// for cloud9 IDE purposes
+// http://jsfiddle.net/3kpFV/
+// http://stackoverflow.com/questions/15348725/how-to-make-quiz-questions-appear-one-at-a-time-in-javascript
+
 $(document).ready(function(){
    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
    $("#introSection").hide()
@@ -9,17 +10,28 @@ $(document).ready(function(){
    $("#introSection").fadeIn(1000*5, function(){
         // fadeIn function
    });
+   $("#questionSpace").hide()
+   var correctCounter = 0,
+   incorrectCounter = 0,
+   unansweredCounter = 0,
+   currentQuestionIndex = 0;
 
-   var congratsMessages = ['Great going cadet'];
+
+
+   var congratsMessages = ['Great going cadet', 'On the money astrophysicist'];
+
 
    $("#startButton").on("click", function() {
      $("#buttonRow").hide();
+     $("#introCard").hide();
+     $("#questionSpace").show();
    })
+
 
 var questions = [
   {
     "q": "How big is the sun?",
-    "c": ["Really Big", "Really Small", "Average"],
+    "c": ["432,288 miles", "553,211 miles", "732.983 miles"],
     "answerIndex": 0
   },
   {
@@ -29,12 +41,30 @@ var questions = [
   }
 ]
 
-$('#question').append("<div>"+ questions[0].q+"</div>");
 
 
 
+function postQuestion(n){
+  $('#question').empty();
+  $('.pickAnswer').remove();
+  $('#question').append("<div>"+ questions[0].q+"</div>");
+  for(var i = 0; i < questions[n].c.length;i++) {
+    $('#choices').append("<div class='pickAnswer'>" + questions[0].c[i]+"</div>");
+  }
+}
 
 
 
+postQuestion(currentQuestionIndex);
+
+function startTrivia() {
+
+}
+
+
+$(".pickAnswer").on("click", function() {
+  currentQuestionIndex++
+  console.log(currentQuestionIndex);
+})
 
  });
